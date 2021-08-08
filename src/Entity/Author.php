560df16d1dc3abba0cb7063ad7cefb293f04ee7f
Eugene Yarosh <yarosh@webant.ru>
2021-08-08 23:25:36 +0800

    Added Author entity

---
+++ src/Entity/Author.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="livejournal_author")
+ * @ORM\Entity()
+ */
+class Author extends BaseEntity
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected int $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected string $name;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected string $blog;
+
+    public function __toString(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBlog(): string
+    {
+        return $this->blog;
+    }
+
+    /**
+     * @param string $blog
+     */
+    public function setBlog(string $blog): void
+    {
+        $this->blog = $blog;
+    }
+}
